dba639430624f9f359b7bc21309b6d6fed301396
shourov98 <mdmahbub0412@gmail.com>
2025-08-15 04:38:38 +0600

    config: rabbitmq configure complete

--- backend/user/src/config/rabbitmq.ts
+++ backend/user/src/config/rabbitmq.ts
@@ -9,7 +9,7 @@ export const connectRabbitMQ = async() => {
             hostname: process.env.RABBITMQ_DEFAULT_HOST,
             port: 5672,
             username: process.env.RABBITMQ_DEFAULT_USER,
-            password: process.env.RABBITMQ_DEFAULT_PASSWORD,
+            password: process.env.RABBITMQ_DEFAULT_PASS,
         })
 
         channel = await connection.createChannel();
@@ -19,4 +19,17 @@ export const connectRabbitMQ = async() => {
         console.log("❌ Failed to connect to RabbitMQ", error);
         
     }
+}
+
+export const publishToQueue = async(queueName: string, message: any) => {
+    if(!channel) {
+        console.log("❌ RabbitMQ channel is not initialized");
+        return;
+    }
+
+    await channel.assertQueue(queueName, {durable: true});
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    })
 }
