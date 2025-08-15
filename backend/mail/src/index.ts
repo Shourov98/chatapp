104a5d09897c15f9fb3a85a2de037ef5567e4061
shourov98 <mdmahbub0412@gmail.com>
2025-08-15 06:16:36 +0600

    mail-config: rabbitmq mail opt service config build

--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,3 +1,13 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express()
+
+const port = process.env.port 
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    
+})
