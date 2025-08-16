d51966e4f54d2898eb2c6515c42785b464c8a4b8
shourov98 <mdmahbub0412@gmail.com>
2025-08-17 05:18:49 +0600

    OTP: otp verification implementation complete.

--- backend/user/src/controllers/user.ts
+++ backend/user/src/controllers/user.ts
@@ -3,6 +3,8 @@ import { redisClient } from "./../index.js";
 
 import TryCatch from "../config/TryCatch.js";
 import { publishToQueue } from "../config/rabbitmq.js";
+import { User } from "../model/User.js";
+import { generateToken } from "../config/generateToken.js";
 
 export const loginUser = TryCatch(async (req, res) => {
     const {email} = req.body
@@ -40,3 +42,42 @@ export const loginUser = TryCatch(async (req, res) => {
 
     })
 })
+
+export const verifyOtp = TryCatch(async(req, res) => {
+    const {email, otp:enteredOtp} = req.body;
+
+    if(!email || !enteredOtp) {
+        res.status(400).json({
+            message: "Email and OTP required",
+        });
+        return;
+    }
+    const otpKey = `otp:${email}`
+
+    const storedOtp = await redisClient.get(otpKey)
+
+    if(!storedOtp || storedOtp !== enteredOtp) {
+        res.status(400).json({
+            message: "Invalid or Expired OTP",
+        });
+        return;
+    }
+
+    await redisClient.del(otpKey);
+
+    let user = await User.findOne({email})
+
+    if(!user) {
+        const name = email.slice(0, email.indexOf("@"));
+        user = await User.create({ name, email});
+    }
+
+    const token = generateToken(user);
+
+    res.json({
+        message: "User verified successfully",
+        user,
+        token,
+    })
+});
+
--- backend/user/src/routes/user.ts
+++ backend/user/src/routes/user.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { loginUser } from '../controllers/user.js';
+import { loginUser, verifyOtp } from '../controllers/user.js';
+
 
 const router = express.Router();
 
-router.post("/login", loginUser)
+router.post("/login", loginUser);
+router.post("/verify", verifyOtp);
 
 export default router;
